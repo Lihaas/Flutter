377b642f6fc3010d52d903458c400d55b1ecedc5
Sahil Verma <vermasahilcf5@gmail.com>
2020-08-14 12:39:24 +0530

    Dice Roll

--- 4.DiceRoller/dice_app/lib/main.dart
+++ 4.DiceRoller/dice_app/lib/main.dart
@@ -1,18 +1,65 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftDice = 3;
+  int rightDice = 2;
+  void changeState() {
+    rightDice = Random().nextInt(5) + 1;
+    leftDice = Random().nextInt(5) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
           appBar: AppBar(
             backgroundColor: Colors.indigo,
             title: Text("Dice Roller"),
+          ),
+          body: Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          changeState();
+                        });
+                      },
+                      child: Image.asset("images/dice$leftDice.png"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          changeState();
+                        });
+                      },
+                      child: Image.asset("images/dice$rightDice.png"),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
